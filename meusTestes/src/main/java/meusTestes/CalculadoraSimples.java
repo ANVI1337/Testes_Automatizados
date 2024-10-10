ba5bb9eaa0c365e0e59f991c81f1e0069f36dd55
Heracton Rutemberg <heracton.farias@aluno.senai.br>
2024-10-09 21:39:49 -0300

    att de teste

--- meusTestes/src/main/java/meusTestes/CalculadoraSimples.java
+++ meusTestes/src/main/java/meusTestes/CalculadoraSimples.java
@@ -1,6 +1,7 @@
 package meusTestes;
 
 public class CalculadoraSimples {
+	private final double dobro = 2;
 	
 	public int add(int numeroA, int numeroB) {
 		return numeroA + numeroB;
@@ -8,6 +9,11 @@ public class CalculadoraSimples {
 	public int multiplicar(int N1, int N2) {
 		return N1 * N2;
 	}
-	
+	public double MetodoDobrarValor(double numero) {
+		return numero * 2;
+	}
+	public double dobroDeUmValor (double valor) {
+		return valor * dobro;
+	}
 	
 }
--- meusTestes/src/main/java/meusTestes/TesteCalculadoraSimples.java
+++ meusTestes/src/main/java/meusTestes/TesteCalculadoraSimples.java
@@ -1,24 +1,55 @@
 package meusTestes;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TesteCalculadoraSimples {
+	// variável não literal
+	private CalculadoraSimples calculadora;
 
+	/*
+	 * Executa um código antes cada teste. utilizado para configurar o ambiente de
+	 * teste (como inicializar objetos) necessários para cada teste.
+	 */
+	@BeforeEach
+	public void setup() {
+		// código de inicialização ( ex:Criar um instância de calculadora)
+		calculadora = new CalculadoraSimples();
+	}
+
+	// -----------------------------------------------------------------------------//
 	@Test
 	void doisMaisDoisExibeQuatro() {
-		
-		var calculadora = new CalculadoraSimples(); // estavamos usando explicitamente o tipo da variável mas será aplicado nesse objeto de forma não explicita usando o "var".
+
+		//var calculadora = new CalculadoraSimples(); // estavamos usando explicitamente o tipo da variável mas será
+													// aplicado nesse objeto de forma não explicita usando o "var".
 		assertEquals(4, calculadora.add(2, 2));
 	}
+
+	@DisplayName("Caso de Teste que exibe a multiplicação de dois vezes tres para retornar 6.")
 	@Test
 	void doisVezesTresRetornaSeis() {
-		var calculadora2 = new CalculadoraSimples();
-		assertEquals(6, calculadora2.multiplicar(2, 3));
- }
-	
-	
-	
-	
+		//var calculadora = new CalculadoraSimples();
+		assertEquals(6, calculadora.multiplicar(2, 3));
+	}
+
+	// -----------------------------------------------------------------------------//
+	@DisplayName("Caso de Teste que exibe a soma de 3 mais 7 resultando em 10.")
+	@Test
+	void tresMaisSeteExibirDez() {
+		//var calculadora = new CalculadoraSimples();
+		assertEquals(10, calculadora.add(3, 7));
+	}
+	// ----------------------------------------------------------------------------//
+
+	@DisplayName("Caso de Teste calcular o dobro de um valor real")
+	@Test
+	void oDobroDeDoisPontoQuatroRetornaQuatroPontoOito() {
+		//var calculadora = new CalculadoraSimples();
+		assertEquals(4.8, calculadora.dobroDeUmValor(2.4)); // metodo com variável aplicada em private final.
+	}
+
 }
